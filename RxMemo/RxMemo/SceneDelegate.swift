db7201aec6ebb9349e48dd5d1d40b479bd9717a7
ebpark9511 <ebpark9511@gmail.com>
2023-04-07 17:07:25 +0900

    first commit

---
+++ RxMemo/RxMemo/SceneDelegate.swift
@@ -0,0 +1,22 @@
+//
+//  SceneDelegate.swift
+//  RxMemo
+//
+//  Created by 박은비 on 2023/04/07.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let _ = (scene as? UIWindowScene) else { return }
+    }
+
+
+
+}
+
